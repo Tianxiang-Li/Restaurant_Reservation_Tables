eda8c4e1371ed0d940d9abb725f5a0b329b82932
TXL-Matthew <tl3091@columbia.edu>
2022-10-14 03:05:07 -0400

    added some sample architecture

---
+++ README.md
@@ -0,0 +1,6 @@
+# Restaurant Reservation: Tables
+
+
+This is the repository for Elastic Beanstalk Application for Table-Management Microservice of Restaurant Reservation.
+
+
---
+++ zip_it.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+zip -r eb-application-package.zip application.py cron.yaml EBSampleApp-Python.iml .ebextensions middleware resources static service_factory.py requirements.txt
+
