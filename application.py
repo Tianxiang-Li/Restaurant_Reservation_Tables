a1af07468169bd973d2741f12d74bd62504bb02f
TXL-Matthew <tl3091@columbia.edu>
2022-12-20 19:50:12 -0500

    add messenger

--- application.py
+++ application.py
@@ -179,13 +179,12 @@ def before_request_func():
 @application.after_request
 def after_request_func(response):
     print('after request executing: Response = ')
-    print(response.get_data())
-    return response
     # send slack message when updating the schema
-    # message = dict()
-    # message['request'] = request.url
-    # message['response'] = response
-    # publish_notification(message)
+    message = dict()
+    message['request'] = request.url
+    message['response'] = response
+    publish_notification(message)
+    return response
 #"""
 
 
