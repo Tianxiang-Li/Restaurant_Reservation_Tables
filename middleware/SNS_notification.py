1ef3ed51c864d883763cc7fca3b3fe14163f8065
TXL-Matthew <tl3091@columbia.edu>
2022-12-20 20:47:59 -0500

    Update SNS_notification.py

--- middleware/SNS_notification.py
+++ middleware/SNS_notification.py
@@ -1,15 +1,23 @@
 import json
+import os
 import boto3
 
+ACCESS_ID = 'AKIARKPHZ57NF3RREITN'
+ACCESS_KEY = 'UIGpae17i9VjS2AcrmNPssAqbSkEjsynVKLsCize'
 
-def publish_notification():
+def publish_notification(request, response):
     sns_topic_arn = "arn:aws:sns:us-east-2:091217326042:TableUpdate"
-    sns_client = boto3.client(
-        'sns',
-        region_name='us-east-2',
-    )
-    #msg = json.dumps(json_message, indent=2)
-    msg = "{\n \"test\": \"From SNS_notification.py\" \n}"
+    sns_client = boto3.client('sns',
+                              region_name='us-east-2',
+                              aws_access_key_id=ACCESS_ID,
+                              aws_secret_access_key=ACCESS_KEY)
+    #if request.method in ['PUT', 'POST', 'DELETE']:
+    msg = {
+        "URL": request.url,
+        "Method": request.method,
+        "Response": response
+    }
+    #msg = "{\n \"test\": \"From SNS_notification.py\" \n}"
     res = sns_client.publish(
         TopicArn=sns_topic_arn,
         Message=msg,
@@ -17,4 +25,4 @@ def publish_notification():
     print("publish_notification response = ",
           json.dumps(res, indent=2))
 
-publish_notification()
+#publish_notification(1, 1)
